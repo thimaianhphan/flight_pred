267ab05cad65eb731d13e7f1ec5273949c554aa1
Thi Mai Anh Phan <anh.phan1@stud.tu-darmstadt.de>
2025-01-13 01:14:06 +0100

    restructure

--- src/monitoring.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-from sklearn.metrics import mean_squared_error
-
-def monitor_model():
-    # Log production metrics
-    mlflow.log_metric("production_mse", mean_squared_error(y_true, y_pred))
-
-if __name__ == "__main__":
-    monitor_model()
